d41b1ff4c9af774d06398287fc4655be3c85efdf
ALLAN SCHRAMM <allanschramm33@gmail.com>
2023-01-18 21:29:20 -0300

    Add project files.

---
+++ Projeto02.GestaoForum/Controllers/AutenticacaoController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Projeto02.GestaoForum.Models;
+
+namespace Projeto02.GestaoForum.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AutenticacaoController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registrar(UsuarioViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                // criando o usuario
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email= model.Email
+                };
+                // incluir o usuario
+                var result = await userManager.CreateAsync(user, model.Senha);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projeto02.GestaoForum/DataAccess/Dao.cs
@@ -0,0 +1,51 @@
+﻿using Dapper.Contrib.Extensions;
+using Projeto02.GestaoForum.Models;
+using System.Data.SqlClient;
+
+namespace Projeto02.GestaoForum.DataAccess
+{
+    public class Dao
+    {
+        string conexao = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=DB_FORUM;Data Source=.";
+
+        public IEnumerable<Forum> ListarForuns()
+        {
+            using (var cn = new SqlConnection(conexao))
+            {
+                return cn.GetAll<Forum>();
+            }
+        }
+
+        public long IncluirForum(Forum forum)
+        {
+            using (var cn = new SqlConnection(conexao))
+            {
+                return cn.Insert<Forum>(forum);
+            }
+        }
+
+        public Forum BuscarForum(int id)
+        {
+            using (var cn = new SqlConnection(conexao))
+            {
+                return cn.Get<Forum>(id);
+            }
+        }
+
+        public bool AlterarForum(Forum forum)
+        {
+            using (var cn = new SqlConnection(conexao))
+            {
+                return cn.Update<Forum>(forum);
+            }
+        }
+
+        public bool RemoverForum(Forum forum)
+        {
+            using (var cn = new SqlConnection(conexao))
+            {
+                return cn.Delete<Forum>(forum);
+            }
+        }
+    }
+}
