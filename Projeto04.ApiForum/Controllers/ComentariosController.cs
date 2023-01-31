2d6c5ddf1ef4ee506a55fab1967a6258432f3a01
ALLAN SCHRAMM <3208077+allanschramm@users.noreply.github.com>
2023-01-31 20:02:23 -0300

    20230130

---
+++ Projeto04.ApiForum/Controllers/ComentariosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Projeto02.GestaoForum.DAL;
+using Projeto02.GestaoForum.Models;
+using Projeto02.GestaoForum.ViewModels;
+
+namespace Projeto04.ApiForum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentariosController : ControllerBase
+    {
+        private readonly ComentariosDao comentariosDao;
+
+        public ComentariosController(ForumContext context)
+        {
+            comentariosDao = new ComentariosDao(context);
+        }
+
+        [HttpGet]
+        public IEnumerable<Comentario> GetComentarios()
+        {
+            return comentariosDao.Listar();
+        }
+
+        [HttpGet("{idForum}")]
+        public IEnumerable<ForumComentariosVM> GetForumComentarios(int idForum)
+        {
+            return comentariosDao.ListarComentariosPorForum(idForum);
+        }
+    }
+}
--- Projeto04.ApiForum/Program.cs
+++ Projeto04.ApiForum/Program.cs
@@ -1,8 +1,12 @@
+using Projeto03.AcessoDados.DI;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager config = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddInfraStructureDb(config);
 
 var app = builder.Build();
 
